907efcef9e5756ae14f5310c84d575f821658056
Jonty-Luo <2437185731@qq.com>
2020-12-30 11:31:43 +0800

    update cpp

--- matrix_block_simd_omp.cpp
+++ matrix_block_simd_omp.cpp
@@ -24,7 +24,7 @@ static unsigned long diff;
 #define FLOAT_SIZE 8 // 一个AVX向量中包含8个单精度浮点数
 #define BYTE_SIZE 16 // 一个AVX向量中包含32个字节
 
-
+#define THREAD_NUMS 24
 
 float rand_float(float s) {
     return 4 * s * (1 - s);
@@ -32,6 +32,8 @@ float rand_float(float s) {
 
 void matrix_gen(float* a, float* b, int n, float seed) {
     float s = seed;
+	omp_set_num_threads(THREAD_NUMS);
+	#pragma omp parallel for
     for (int i = 0; i < n * n; ++i) {
         s = rand_float(s);
         a[i] = s;
@@ -142,11 +144,11 @@ void matrix_mul_bk_avx(int n, int block_size, float* A, float* B, float* C) {
 	}
 }
 
-void matrix_mul_bk_avx_multhread(int n, int block_size, float* A, float* B, float* C, int thread_num)
+void matrix_mul_bk_avx_multhread(int n, int block_size, float* A, float* B, float* C)
 {
-	omp_set_num_threads(thread_num);
+	omp_set_num_threads(THREAD_NUMS);
 	#pragma omp parallel for
-	for (int sj = 0; sj < n/2; sj += block_size) {
+	for (int sj = 0; sj < n; sj += block_size) {
 		for (int si = 0; si < n; si += block_size) {
 			for (int sk = 0; sk < n; sk += block_size) {
 				int i, j;
@@ -154,22 +156,20 @@ void matrix_mul_bk_avx_multhread(int n, int block_size, float* A, float* B, floa
 				float* b1 = B + sk * n + sj;
 				float* c1 = C + si * n + sj;
 				// 为使用SIMD指令，对b1进行转置
-				float* b1_t = (float*)_mm_malloc(sizeof(float) * block_size * block_size, 64);
-				for (int ti = 0; ti < block_size; ++ti) {
-					for (int tj = 0; tj < block_size; ++tj) {
-						b1_t[tj * block_size + ti] = b1[ti * block_size + tj];
-					}
-				}
+				// float* b1_t = (float*)_mm_malloc(sizeof(float) * block_size * block_size, 64);
+				// for (int ti = 0; ti < block_size; ++ti) {
+				// 	for (int tj = 0; tj < block_size; ++tj) {
+				// 		b1_t[tj * block_size + ti] = b1[ti * block_size + tj];
+				// 	}
+				// }
 				for (i = 0; i < block_size; ++i) {
 					for (j = 0; j < block_size; ++j) {
 						//加载数组当前元素的寄存器，每次读取8个单精度浮点数
-						__m256 va1 = _mm256_setzero_ps();
-						__m256 vb1 = _mm256_setzero_ps();
-						__m256 vc1 = _mm256_setzero_ps();
-						__m256 sum_vc = _mm256_setzero_ps();
+						__m256 va1, vb1, vc1, sum_vc;
+						//__m256 sum_vc = _mm256_setzero_ps();
 						for (int m = 0; m < block_size; m += FLOAT_SIZE) {
 							va1 = _mm256_load_ps(&a1[i * block_size + m]);
-							vb1 = _mm256_load_ps(&b1_t[j * block_size + m]);
+							vb1 = _mm256_load_ps(&b1[j * block_size + m]);
 							//sum_vc = _mm256_fmadd_ps(va1, vb1, sum_vc);
 							sum_vc = _mm256_add_ps(_mm256_mul_ps(va1, vb1), sum_vc);
 						}			
@@ -180,18 +180,8 @@ void matrix_mul_bk_avx_multhread(int n, int block_size, float* A, float* B, floa
 					}
 				}
 				// 释放_mm_malloc分配的内存
-				_mm_free(b1_t);
+				//_mm_free(b1_t);
 			}
-		}
-	}
-}
-
-//数组转置存储
-void rotate(float** b, float** copy_b, int M) {
-	int i, j;
-	for (i = 0; i < M; i++) {
-		for (j = 0; j < M; j++) {
-			copy_b[j][i] = b[i][j];
 		}
 	}
 }
@@ -246,7 +236,7 @@ void run_solution_base(int n, int m)
 	delete[] C;
 }
 
-void run_solution_simd_thread(int n, int m, int threadnum)
+void run_solution_simd_thread(int n, int m)
 {
 	// 在堆中存放矩阵
 	float* A = (float*)_mm_malloc(sizeof(float) * n * n, 64); // 按64字节对齐
@@ -259,7 +249,7 @@ void run_solution_simd_thread(int n, int m, int threadnum)
 	gettimeofday(&startTime, NULL);
 
 	// 矩阵相乘
-	matrix_mul_bk_avx_multhread(n, m, A, B, C, threadnum);
+	matrix_mul_bk_avx_multhread(n, m, A, B, C);
 
 	gettimeofday(&endTime, NULL);
 	diff = 1000 * (endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1000;
@@ -278,14 +268,14 @@ int main()
 	// 	}
 	// }
 	
-	for (int i = 0; i < 4; ++i) {
+	for (int i = 3; i < 4; ++i) {
 		for (int j = 0; j < 5; ++j) {
-			run_solution_simd_thread(matrixN[i], blockM[j], 24);	
+			run_solution_simd_thread(matrixN[i], blockM[j]);	
 		}
 	}
 	int index = 0;
-	cout << "Thread num: " << 4 << endl;
-	for (int i = 0; i < 4; ++i) {
+	cout << "Thread num: " << THREAD_NUMS << endl;
+	for (int i = 3; i < 4; ++i) {
 		for (int j = 0; j < 5; ++j) {
 			// 输出计时结果，以毫秒为单位
 			cout << "N = " << matrixN[i] << ", M = " << blockM[j] << " multiple thread: "
